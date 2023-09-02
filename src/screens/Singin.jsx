26be17e118f8d33fb06143117086c831d2ea86d4
Merok23 <alejocalizblanco@gmail.com>
2023-09-02 18:18:26 -0300

    Se agrego la dependencia axios al package json para poder hacer fetchs Se saco body de scss de sing in Se saco body de scss de index.css Se agrego el codigo para que cuando se aprete singin se le pegue a la API de back end

--- src/screens/Singin.jsx
+++ src/screens/Singin.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import pingHandler from '../service/Ping';
 import '../styles/singin.scss';
 
 const Singin = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const handlePingRequest = async () => {
+        try {
+            const res = await pingHandler();
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
   return (
-    <div className="background">
-        <div>
-            <h1 className="title"> Welcome back! </h1>
-        </div>
-        <div className="center">
-            <button className="google-sign-in-button">Continue with Google</button>
-        </div>
-        <input className="center" type="email" placeholder="Email or Username" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <input className="center" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button className="sign-in-button">Sign in</button>
-        <Link to="/">Go to Home</Link>
+    <div className="top-centre">
+      <div className="background">
+          <div>
+              <h1 className="title"> Welcome back! </h1>
+          </div>
+          <div className="center">
+              <button className="google-sign-in-button">Continue with Google</button>
+          </div>
+          <input className="center" type="email" placeholder="Email or Username" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+          <input className="center" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+          <button className="sign-in-button" onClick={handlePingRequest}>Sign in</button>
+          <Link to="/">Go to Home</Link>
+      </div>
     </div>
   );
 };
