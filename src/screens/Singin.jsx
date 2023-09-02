960e145b65a9bd03ffd625574791aa87ca138d9d
Merok23 <alejocalizblanco@gmail.com>
2023-09-02 14:15:40 -0300

    Se agrego el campo de email y password y estoy luchando con sl scss

--- src/screens/Singin.jsx
+++ src/screens/Singin.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/singin.scss"
+import '../styles/singin.scss';
 
 const Singin = () => {
-    return (
-        <div className = "background">
-            <p>hola</p>
-            <Link to="/">Go to Home</Link>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  return (
+    <div className="background">
+        <div>
+            <h1 className="title"> Welcome back! </h1>
         </div>
-        
-    )
-}
+        <div className="center">
+            <button className="google-sign-in-button">Continue with Google</button>
+        </div>
+        <input className="center" type="email" placeholder="Email or Username" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <input className="center" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+        <button className="sign-in-button">Sign in</button>
+        <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
 
-
-export default Singin
+export default Singin;
