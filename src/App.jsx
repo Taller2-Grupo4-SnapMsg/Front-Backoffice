e8c675ad0dc1e5da0e9727597dfaeb38a93746b5
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-09-02 20:48:32 -0300

    modificaciones en los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import About from './screens/About';
 
 function App() {
   return (
-    <Router>
-      <Routes> 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <Home />
   );
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import small_logo from '/small_logo.png';
+//import "../styles/home.scss";
 
 const Home = () => {
   return (
-    <div className="singin">
-      <p>home</p>
-      <Link to="/about">Go to About</Link>
+    <div className="home">
+      <div className="centered-container">
+        <p className="centered-text">Texto centrado</p>
+      </div>
+
+      <label className="home-title">Hdhdhhd</label>
+
+      <div className="lateral_menu">
+        <img src={small_logo} className="logo-img" alt="SnapMessage logo" />
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+/*const Home = () => {
+  const [text, setText] = useState('')
+  return (
+    <div className="home">
+      <label></label>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+    </div>
+  );
+};*/
