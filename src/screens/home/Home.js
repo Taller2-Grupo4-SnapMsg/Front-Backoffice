c905304bbcfe4e1b4928620892211c7ca6b443b8
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-09-03 16:23:33 -0300

    fracione home en archivos feed y menu

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,7 +4,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
-//import Feed from './Feed';
+import Feed from './Feed';
+import Menu from './Menu';
 import "../../styles/home/home.scss";
 import "../../styles/home/container-1.scss";
 import "../../styles/home/container-2.scss";
@@ -52,106 +53,10 @@ const Home = () => {
 
   return (
     <div className="home">
-
-      <div className="container-1">
-        
-        <div className="logo-container">
-          <img src='/small_logo.png' className="logo-app" alt="SnapMessage logo" />
-          <span className="text-app">SnapMessage</span>
-        </div>
-
-        <div className="menu">
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">Home</span>
-          </div>
-          <div className="item">
-          <img src="/home.png" alt="Icono 2" className="icon"/>
-            <span className="text">Explore</span>
-          </div>
-          <Link to="/notifications" className="text">
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">Notifications</span>
-          </div>
-          </Link>
-          <Link to="/notifications" className="text">
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">Messages</span>
-          </div>
-          </Link>
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">Profile</span>
-          </div>
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">Settings</span>
-          </div>
-          <div className="item">
-            <img src="/home.png" alt="Icono 1" className="icon"/>
-            <span className="text">More</span>
-          </div>
-        </div>
-
-      </div>
-
-      <div className="container-2">
-
-        <div className="home-title-container">
-          <div className="text-container">
-          <label className="home-title">Home</label>
-          </div>
-        </div>
-
-        <div className="header">
-        </div>
-
-        <div className="box-to-tweet">
-          <div className="text-box">
-              <textarea
-                className="text"
-                placeholder="What is happening?"
-                value={text}
-                onChange={updateText}
-                //onkeydown="miFuncionOnKeyDown(event)"
-              />
-              <button className="post-button" onClick={handleSubmit}>Post</button>
-          </div>
-        </div>
-
-        <div className="feed">
-          {publicacionesHarcodeadas.map((publicacion) => (
-            <div className="post" key={publicacion.id}>
-              <div className="profile-information">
-                <img
-                  className="profile-picture"
-                  src={publicacion.foto}
-                  alt="Foto de perfil"
-                />
-                <div className="profile-name">{publicacion.nombre}</div>
-              </div>
-              <div className="post-text">{publicacion.text}</div>
-              {publicacion.imagenUrl && (
-                <img
-                  className="post-picture"
-                  src={publicacion.imagenUrl}
-                  alt="Imagen de la publicación"
-                />
-              )}
-              <div className="actions">
-                {}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
+      < Menu/>
+      < Feed/>
       <div className="container-3">
-
       </div>
-
     </div>
   );
 };
