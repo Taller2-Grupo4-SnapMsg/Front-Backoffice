e8b55a8ba3d3a34b7ac3c23fb9ee310f9c282b56
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-09-08 16:31:55 -0300

    separamos navBar y terminamos sacando el contenedor Drawer
    Co-authored-by: Nathalia Encinoza <71742037+nathencinoza@users.noreply.github.com>

---
+++ src/components/navegationBar/navegationBar.jsx
@@ -0,0 +1,102 @@
+
+import Drawer from '@mui/material/Drawer';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+// Importa los iconos
+import MessagesIcon from '@mui/icons-material/Email';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+// Import your custom Logo component
+import Logo from '../../components/logo/Logo';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+export const DefaultTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#353839',
+        navBar: '#353839',
+        light: '#F5EBFF',
+      },
+      mode: 'dark',
+    },
+  });
+
+const spacerStyles = {
+    marginTop: '50px', // Increase this value for more vertical space
+  };
+  
+  const logoStyles = {
+    textAlign: 'center',
+    margin: '20px 0', // Adjust the margin as needed
+    '& img': {
+      maxWidth: '50px', // Adjust the maximum width as needed
+    },
+  };
+  
+  const listItemButtonStyle = {
+    borderRadius: '24px',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+    display: 'flex', // Added
+    alignItems: 'center', // Vertically center content
+    padding: '8px 16px', // Adjust padding as needed
+  };
+
+  /*    <Drawer
+          sx={{
+            width: '300px',
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: '300px',
+              boxSizing: 'border-box',
+
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >*/
+//</Drawer>
+
+const NavegationBar = () => {
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <Box component="navBar"
+        sx={{bgcolor: '#000000'}}>
+      <Toolbar />
+        <Box sx={logoStyles}>
+          <Logo />
+        </Box>
+        <List position="fixed" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+          {['Home', 'Explore', 'Notifications', 'Messages', 'Profile', 'Settings'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={spacerStyles}>
+              <ListItemButton sx={listItemButtonStyle}>
+                <ListItemIcon>
+                  {index === 0 && <HomeIcon />}
+                  {index === 1 && <SearchIcon />}
+                  {index === 2 && <NotificationsActiveIcon />}
+                  {index === 3 && <MessagesIcon />}
+                  {index === 4 && <PersonIcon />}
+                  {index === 5 && <SettingsIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      </ThemeProvider>
+  );
+};
+
+export default NavegationBar;
--- src/screens/profile/Profile.jsx
+++ src/screens/profile/Profile.jsx
@@ -1,134 +1,77 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 
 import Avatar from '@mui/material/Avatar';
-import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
+import NavegationBar from '../../components/navegationBar/navegationBar';
 
+//<CssBaseline />
 
-
-// Importa los iconos
-import MessagesIcon from '@mui/icons-material/Email';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-
-// Import your custom Logo component
-import Logo from '../../components/logo/Logo';
-import logo from '../../components/logo/Logo';
-
-
-const defaultTheme = createTheme({
+export const DefaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#353839',
+      main: '#000000',
+      navBar: '#000099',
       light: '#F5EBFF',
     },
     mode: 'dark',
   },
 });
 
-const spacerStyles = {
-  marginTop: '50px', // Increase this value for more vertical space
-};
-
-const logoStyles = {
-  textAlign: 'center',
-  margin: '20px 0', // Adjust the margin as needed
-  '& img': {
-    maxWidth: '50px', // Adjust the maximum width as needed
-  },
-};
-
-const listItemButtonStyle = {
-  borderRadius: '24px',
-  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-  display: 'flex', // Added
-  alignItems: 'center', // Vertically center content
-  padding: '8px 16px', // Adjust padding as needed
-};
-
-
 export default function PermanentDrawerLeft() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex', bgcolor: '#947EB0' }}>
-        <CssBaseline />
-        <Drawer
-          sx={{
-            width: '300px',
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: '300px',
-              boxSizing: 'border-box',
+    <ThemeProvider theme={DefaultTheme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', bgcolor: '#353839' }}>
+        <NavegationBar/>
 
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-        >
-          <Toolbar />
-          <Box sx={logoStyles}>
-            <Logo />
-          </Box>
-          <List position="fixed" sx={{ marginLeft: '20px', marginRight: '20px' }}>
-            {['Home', 'Explore', 'Notifications', 'Messages', 'Profile', 'Settings'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={spacerStyles}>
-                <ListItemButton sx={listItemButtonStyle}>
-                  <ListItemIcon>
-                    {index === 0 && <HomeIcon />}
-                    {index === 1 && <SearchIcon />}
-                    {index === 2 && <NotificationsActiveIcon />}
-                    {index === 3 && <MessagesIcon />}
-                    {index === 4 && <PersonIcon />}
-                    {index === 5 && <SettingsIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
+        
         <Box
           component="main"
-          sx={{ flexGrow: 1, bgcolor: '#947EB0', p: 3 }}
-        >         
-          <Toolbar />
-          <Avatar
-            sx={{ width: 100, height: 100, margin: '0 auto' }}
-          >
-            J
-          </Avatar>
-          <Typography variant="h4" sx={{ textAlign: 'center', mt: 2 }}>
-            Taylor Swift
-          </Typography>
-          <Typography variant="subtitle1" sx={{ textAlign: 'center', color: 'text.secondary' }}>
-            @TaylorSwift
-          </Typography>
-          <Typography variant="body1" sx={{ textAlign: 'center', mt: 2 }}>
-            jsdjfsdjfslkfjslka
-            ajsdnakjsdnalksndsa
-            asjkdaksjdnaksjdnsa
-          </Typography>
-          <Divider sx={{ mt: 3, mb: 2 }} />
-          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
-            1 Followers
-          </Typography>
+          sx={{ flexGrow: 1, bgcolor: '#000000', p: 3 }}
+        >   
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              {/* Contenido de la primera columna */}
+              <Divider sx={{ mr: 3 }} />
+              <Toolbar />
+              <Avatar
+                sx={{ width: 100, height: 100, margin: '0 auto' }}
+              >
+                J
+              </Avatar>
+              <Typography variant="h4" sx={{ textAlign: 'center', mt: 2 }}>
+                Taylor Swift
+              </Typography>
+              <Typography variant="subtitle1" sx={{ textAlign: 'center', color: 'text.secondary' }}>
+                @TaylorSwift
+              </Typography>
+              <Typography variant="body1" sx={{ textAlign: 'center', mt: 2 }}>
+                jsdjfsdjfslkfjslka
+                ajsdnakjsdnalksndsa
+                asjkdaksjdnaksjdnsa
+              </Typography>
+              <Divider sx={{ mt: 3, mb: 2 }} />
+              <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
+                1 Followers
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              {/* Contenido de la segunda columna */}
+              {/* Agrega aquí el contenido de la segunda columna */}
+            </Grid>
+
+          </Grid>
         </Box>
+
+
+
       </Box>
     </ThemeProvider>
   );
